159e24c53fe93e1148fcd383c7db7bac9d330e7f
seoyerin <seyerin12@naver.com>
2025-10-31 22:34:39 +0900

    feat: 아이콘 받아오는 함수 작성

---
+++ src/svg.d.ts
@@ -0,0 +1,10 @@
+declare module "*.svg?react" {
+  import type * as React from "react";
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
+
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
