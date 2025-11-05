136731595c91d1966660e6327c4e57b510ad2c4e
seoyerin <seyerin12@naver.com>
2025-11-05 23:05:51 +0900

    chore: 아이콘 추가

---
+++ src/features/groupEntry/ui/groupEntry.tsx
@@ -0,0 +1,15 @@
+export function GroupEtry() {
+  return (
+    <main>
+      <p>반려 기록 시작하기</p>
+      <div>
+        <div>
+          <div>
+            <p>그룹 만들기</p>
+          </div>
+          <p>그룹을 만들고 다른 보호자를 초대해 보세요</p>
+        </div>
+      </div>
+    </main>
+  );
+}
--- src/shared/getIcon/icons.ts
+++ src/shared/getIcon/icons.ts
@@ -2,3 +2,6 @@ export { default as DefaultProfile } from "@/assets/defaultProfile.svg?react";
 export { default as Female } from "@/assets/female.svg?react";
 export { default as Male } from "@/assets/male.svg?react";
 export { default as PrevBtn } from "@/assets/prevBtn.svg?react";
+export { default as Key } from "@/assets/key.svg?react";
+export { default as Group } from "@/assets/group.svg?react";
+export { default as RightArrow } from "@/assets/rightArow.svg?react";
