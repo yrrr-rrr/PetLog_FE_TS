efe82ed86879162971d0cdc1e0dd5cb949568e6b
seoyerin <seyerin12@naver.com>
2025-11-18 19:29:00 +0900

    feat: 날짜별로 일지 분류하는 함수

---
+++ src/features/diary/home/lib/sortByDate.ts
@@ -0,0 +1,16 @@
+import type { DiaryType } from "../store/diaryStore";
+
+export function sortByDate(arr: DiaryType[]) {
+  if (arr.length < 1) {
+    return [];
+  }
+
+  return arr.reduce<Record<string, DiaryType[]>>((acc, currentValue) => {
+    const date = currentValue.writtenAt.toString();
+    if (!acc[date]) {
+      acc[date] = [];
+    }
+    acc[date].push(currentValue);
+    return acc;
+  }, {});
+}
