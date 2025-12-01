2a1b4839ad49a0b3045d796fb5f9edd83474e548
seoyerin <seyerin12@naver.com>
2025-12-01 22:56:45 +0900

    feat: 이미지 배열 얻는 함수 추가

---
+++ src/features/diary/add/lib/getImgs.ts
@@ -0,0 +1,16 @@
+import type { ImgType } from "../store/addStore";
+
+export function getNewImgs(imgs: ImgType[]) {
+  const newImg = imgs
+    .map((x) => x.file)
+    .filter((x): x is File => x !== undefined);
+  return newImg;
+}
+
+export function getExistImgs(imgs: ImgType[]) {
+  const existImg = imgs
+    .filter((x) => x.status == "existing")
+    .map((x) => x.s3Url)
+    .filter((x): x is string => x !== undefined);
+  return existImg;
+}
