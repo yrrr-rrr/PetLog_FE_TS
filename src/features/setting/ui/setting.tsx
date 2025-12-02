81bdefca8154c6b01934637b644624d9de56b56e
seoyerin <seyerin12@naver.com>
2025-12-02 17:12:38 +0900

    refactor: 로그아웃, 탈퇴, 그룹 나가기 버튼 클릭 시 재확인 모달 활성화

--- src/features/setting/ui/setting.tsx
+++ src/features/setting/ui/setting.tsx
@@ -9,13 +9,33 @@ import { BackButton } from "@/shared/backBtn/BackButton";
 import * as s from "./style";
 import { getNotification } from "../lib/getNotification";
 import { getGroupId } from "@/shared/getGroupid/getGroupId";
+import { BaseModal } from "@/shared/baseModal/ui/baseModal";
+import { useModal } from "@/shared/baseModal/store/modalStroe";
+
+type ModalKeyType = "deleteAccount" | "logout" | "leaveGroup";
 
 export function Setting() {
   const { openModal } = useWarningModal();
+  const { isOpen, setIsOpen } = useModal();
   const [toggle, setToggle] = useState(false);
   const nav = useNavigate();
   const acc = localStorage.getItem("acc");
   const [groupId, setGroupId] = useState(0);
+  const [modalKey, setModalKey] = useState<ModalKeyType>("deleteAccount");
+  const modalMessage = {
+    deleteAccount: {
+      message: "회원을 탈퇴 하시겠습니까?",
+      action: () => deleteAccount(),
+    },
+    logout: {
+      message: "로그아웃 하시겠습니까?",
+      action: () => logout(nav),
+    },
+    leaveGroup: {
+      message: "그룹을 나가시겠습니까?",
+      action: () => leaveGroup(openModal, acc ? acc : "", groupId, nav),
+    },
+  };
 
   useEffect(() => {
     if (!acc) {
@@ -32,63 +52,61 @@ export function Setting() {
   }, [acc]);
 
   return (
-    <>
-      <s.Main>
-        <BackButton
-          onClick={() => {
-            nav(-1);
-          }}
-        >
-          설정
-        </BackButton>
-        <s.Ul>
-          <s.Li>
-            <p>알림 수신 여부</p>
-            <s.Toggle
-              $toggle={toggle}
-              onClick={() => {
-                if (!acc) {
-                  return;
-                }
-                setNotification(!toggle, openModal, acc);
-                setToggle((prev) => !prev);
-              }}
-            >
-              <div></div>
-            </s.Toggle>
-          </s.Li>
-          <s.Li
+    <s.Main>
+      <BackButton
+        onClick={() => {
+          nav(-1);
+        }}
+      >
+        설정
+      </BackButton>
+      <s.Ul>
+        <s.Li>
+          <p>알림 수신 여부</p>
+          <s.Toggle
+            $toggle={toggle}
             onClick={() => {
               if (!acc) {
                 return;
               }
-              leaveGroup(openModal, acc, groupId, nav);
+              setNotification(!toggle, openModal, acc);
+              setToggle((prev) => !prev);
             }}
           >
-            그룹 나가기
-          </s.Li>
-          <s.Li
-            onClick={() => {
-              if (!acc) {
-                return;
-              }
-              logout(nav);
-            }}
-          >
-            로그 아웃
-          </s.Li>
-        </s.Ul>
-        <s.WithdrawBox
+            <s.ToggleBtn></s.ToggleBtn>
+          </s.Toggle>
+        </s.Li>
+        <s.Li
           onClick={() => {
-            if (!acc) {
-              return;
-            }
-            deleteAccount();
+            setModalKey("leaveGroup");
+            setIsOpen();
           }}
         >
-          탈퇴 하기
-        </s.WithdrawBox>
-      </s.Main>
-    </>
+          그룹 나가기
+        </s.Li>
+        <s.Li
+          onClick={() => {
+            setModalKey("logout");
+            setIsOpen();
+          }}
+        >
+          로그 아웃
+        </s.Li>
+      </s.Ul>
+      <s.WithdrawBox
+        onClick={() => {
+          setModalKey("deleteAccount");
+          setIsOpen();
+        }}
+      >
+        탈퇴 하기
+      </s.WithdrawBox>
+      {isOpen && (
+        <BaseModal
+          message={modalMessage[modalKey].message}
+          onClick={modalMessage[modalKey].action}
+        />
+      )}
+    </s.Main>
   );
 }
